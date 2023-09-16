eb31ab7fac1c569c853da6f4dfa3b3b6432b221b
merilleo <merilleo@freenet.de>
2023-09-16 21:49:37 +0200

    feat(Resizer): added context menu resizer

--- src/lib/contextmenu/ContextMenu.svelte
+++ src/lib/contextmenu/ContextMenu.svelte
@@ -1,14 +1,25 @@
 <script lang="ts">
+    import Resizer from "./Resizer.svelte";
+
+    let width: number = 300;
 
 </script>
 
-<div class="contextmenu bg-mid">
-    <slot></slot>
+<div class="contextmenu bg-mid "
+     style:width="{width}px">
+    <div class="contextmenu-container">
+        <slot></slot>
+    </div>
+    <Resizer bind:width={width} />
 </div>
 
 <style>
     .contextmenu {
-        width: 300px;
         height: 100%;
+        display: flex;
+
+    }
+    .contextmenu-container {
+        width: 100%;
     }
 </style>
