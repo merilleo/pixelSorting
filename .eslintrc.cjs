4c2e9f90b9b534c46b720649359b5aa4cffd81f2
merilleo <merilleo@freenet.de>
2024-02-10 09:39:28 +0100

    Update ESLint config and add Svelte support
    Replace .eslintrc.json with .eslintrc.cjs, updating and extending linting rules and adding support for Svelte. Configure vite to use svelte-preprocess and update package.json to include new linting dependencies and script.

---
+++ .eslintrc.cjs
@@ -0,0 +1,144 @@
+module.exports = {
+  // Specifies that the environment is a browser and is running a version of ECMAScript that is compatible with ECMAScript 2021 standards.
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+
+  // Extends the standard recommendations by ESLint, TypeScript, and Svelte.
+  'extends': [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:svelte/recommended'
+  ],
+
+  // Specifies the parser to be used.
+  'parser': '@typescript-eslint/parser',
+
+  // Parser options are for advanced parsing feature configurations.
+  'parserOptions': {
+    'ecmaVersion': 2023, // The version of ECMAScript syntax to be used.
+    'sourceType': 'module', // The source code is in the form of ECMAScript modules.
+    'extraFileExtensions': ['.svelte'], // Includes .svelte files for parsing.
+    'project': './tsconfig.json',
+  },
+
+  // Specifies special parsing for *.svelte files.
+  'overrides': [
+    {
+      'files': ['*.svelte'],
+      'parser': 'svelte-eslint-parser',
+      'parserOptions': {
+        'parser': '@typescript-eslint/parser'
+      }
+    }
+  ],
+
+  // Specifies the plugin to be used.
+  'plugins': [
+    '@typescript-eslint',
+    'check-file'
+  ],
+
+  // Specifies the rules to be followed.
+  'rules': {
+    // Enforces strong typing and readability with booleans.
+    '@typescript-eslint/strict-boolean-expressions': [
+      'error',
+      {
+        'allowString': false,
+        'allowNumber': false
+      }
+    ],
+    // Enforces naming conventions for classes to be in PascalCase.
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        'selector': 'class',
+        'format': [
+          'PascalCase'
+        ]
+      }
+    ],
+    // Enforces consistence in type imports.
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      {
+        'prefer': 'type-imports'
+      }
+    ],
+    // Prevents the use of a variable before it was defined.
+    '@typescript-eslint/no-use-before-define': ['error'],
+    // Provides a warning for inconsistent use of delimiters in member expressions.
+    '@typescript-eslint/member-delimiter-style': [
+      'warn',
+      {
+        'multiline': {
+          'delimiter': 'comma',
+          'requireLast': false
+        },
+        'singleline': {
+          'delimiter': 'comma',
+          'requireLast': false
+        }
+      }
+    ],
+    // Enforces the usage of language tags in Svelte blocks.
+    'svelte/block-lang': [
+      'error',
+      {
+        'enforceScriptPresent': true,
+        'script': ['ts', null]
+      }
+    ],
+    // Enforce naming convention TypeScript and Svelte files.
+    "check-file/filename-naming-convention": [
+      "error",
+      {
+        "**/*.{ts,svelte}": "PASCAL_CASE",
+      }
+    ],
+    // Enforce naming convention for all directories inside the `src` directory.
+    "check-file/folder-naming-convention": [
+      "error",
+      {
+        "src/**/": "KEBAB_CASE",
+      }
+    ],
+        // Enforces the use of === and !== over == and !=.
+    'eqeqeq': 2,
+    // Enforces consistent use of curly braces.
+    'curly': 1,
+    // Warns when a defined variable is not used.
+    'no-unused-vars': [
+      'warn',
+      {
+        'vars': 'all',
+        'args': 'after-used',
+        'ignoreRestSiblings': false
+      }
+    ],
+    // Enforces the usage of semicolons.
+    'semi': ['error','always'],
+    // Enforces the usage of single quotes.
+    'quotes': [
+      'error',
+      'single',
+      {
+        'avoidEscape': true,
+        'allowTemplateLiterals': true
+      }
+    ],
+    // Enforces the usage of trailing commas.
+    'comma-dangle': [
+      'error',
+      {
+        'functions': 'never',
+        'imports': 'never',
+        'exports': 'only-multiline',
+        'arrays': 'only-multiline',
+        'objects': 'only-multiline'
+      }
+    ]
+  }
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import sveltePreprocess from 'svelte-preprocess';
 
 // https://vitejs.dev/config/
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+      svelte({
+        preprocess: sveltePreprocess(),
+      })
+  ],
 })
